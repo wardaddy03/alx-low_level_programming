6ac15b16393b755c7377108b56299961d2fffe90
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-07 12:42:09 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - a c program that prints a line to the standard error
- * Return 1 (success)
+ * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
 	return (1);
 }
