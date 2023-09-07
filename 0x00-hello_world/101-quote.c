cb824e3f69b2f8800c4f3a7dbf3f519fd1ccebe4
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-07 12:52:31 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c program that prints a line to the standard error
- * Return: 1 (success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a new line, to standard error.
+ * Return: Always 1
  */
+
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	write(2, "and that piece of art is useful\" - Dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
