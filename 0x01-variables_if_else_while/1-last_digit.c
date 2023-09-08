2690aae60c90fb62476a737a97e9e97f541e7198
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-08 15:13:34 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,36 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-#include <stdio.h>
-
 /**
- * main  - Prints a text according to a number
- * Return: Always (Success)
+ * main - function to check the size of last digit of random
+ * number
  *
+ * Return: 0
  */
 
 int main(void)
-
 {
-	int n;
-        int lastd;
+	int n, l;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2
+	n = rand() - RAND_MAX / 2;
 
-	if (lastd > 5)
+	l = n % 10;
+
+	if (l > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
-	else if (lastd == 0)
+	else if (l == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, lastd);
+		printf("Last digit of %d is %d and is 0\n", n, l);
 	}
-	else if (lastd < 6 && lastd != 0)
+	else if (l < 6 && l != 0)
 	{
-	printf("last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
+
 	return (0);
 }
 
