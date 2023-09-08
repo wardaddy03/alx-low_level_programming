ea73b814bc4f631ab344a6c11c6e05e2d4ec684e
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-08 13:48:08 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
+
 /**
- * main - Entry point
- * Return: Always 0 (success)
+ * main - Prints if number is positive, zero or negative
+ * Return: Always (Success)
  *
  */
 
@@ -14,11 +17,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if  (n > 0)
-		printf("%d is positive\n", n);
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n);
+	}
 	else if (n == 0)
-		printf("%d is zero\n", n);
+	{
+	printf("%d is zero\n", n);
+	}
 	else
-	        printf("%d is negative\n", n);
+	{
+	printf("%d is negative\n", n);
+	}
 	return (0);
 }
