b9d97fda73f94f433d62b94e72ac234dc12df358
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-25 18:18:30 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,12 +4,13 @@
  * _memcpy - function to copy memory area
  * @dest: memory where is stored
  * @src: memory where is copied
- * @n: number of bytes 
+ * @n: number of bytes
  *
  * Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
 	int a = 0;
 	int b = n;
