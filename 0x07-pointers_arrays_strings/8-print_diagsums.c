922b84edd237fed7f13196ddc971ee61dfa5ac81
wardaddy03 <emmanuel.idehen@elizadeuniversity.edu.nggit config --global -egit config --global user.email emmanuel.idehen@elizadeuniversity.edu.ng>
2023-09-25 19:03:16 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of 2 diagonals of a square matrix
+ * of integers.
+ * @a: array
+ * @size: size of an array
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	long int j = 0, k = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		u = u + *(a + i * size + i);
+		v = v + *(a + i * size - i);
+	}
+
+	printf("%ld, %ld\n", u, v);
+}
